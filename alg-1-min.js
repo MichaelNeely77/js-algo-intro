88d02aa99237a09f29878810749b2e7319bc7a3b
Michael Neely <michael@michaelneely.net>
2023-12-20 11:51:33 -0500

    Added my version of even or odd algorithm

--- alg-1-min.js
+++ alg-1-min.js
@@ -14,9 +14,33 @@ function getMin(numbers) {
     return currentMinimum;
 }
 
+function getMin2(numbers) {
+    if (!numbers.length) {
+        throw new Error('Should not be an empty array');
+    }
+    for(let i = 0; i < numbers.length; i++) {
+        let outerElement = numbers[i];
+        for (let j = i + 1; j < numbers.length; j++) {
+        let innerElement = numbers[j];
+            if (outerElement > innerElement) {
+                numbers[i] = innerElement;
+                numbers[j] = outerElement;
+
+                outerElement = numbers[i];
+                innerElement = numbers[j];
+                console.log(numbers[i], numbers[j]);
+            }
+        }
+    }
+
+    return numbers[0];
+}
+
 const testNumbers = [20, 15, -5, 2, 30, -100];
 
-const min = getMin(testNumbers);
 
+const min = getMin2(testNumbers);
 console.log(min);
+
+
 
---
+++ alg-2-even-odd.js
@@ -0,0 +1,10 @@
+// Create function that answers whether an number is even or odd
+
+function isEvenOrOdd(number) {
+    if (number % 2 === 0 )  {
+        return 'Number is even';
+    } else {
+        return 'Number is odd'
+    }
+}
+
